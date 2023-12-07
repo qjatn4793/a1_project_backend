ce89635c21bd0ecf082b5cd3a4771f27fce099c1
김범수 <qjatn4793@naver.com>
2023-12-07 16:56:58 +0900

    [2023-12-07] 김범수 - 파일업로드 엔드포인트 기능 구현

--- src/main/java/com/project/a1/controller/MainController.java
+++ src/main/java/com/project/a1/controller/MainController.java
@@ -5,10 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.HttpEntity;
 
 // api 통신
@@ -46,6 +48,8 @@ public class MainController {
 	@GetMapping("/searchItem")
     public String searchItem(@RequestParam String item) {
 		
+		/* 임시로 주석 처리 */
+		/*
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.add("X-Naver-Client-Id", naverClientKey);
@@ -61,8 +65,19 @@ public class MainController {
     	// 네이버 응답 값
     	String result = responseEntity.getBody();
     	logger.info("naver : {}", result);
+    	*/
     	
         return "results : " + item;
+    }
+	
+	@PostMapping("/searchItem")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file) {
+
+        // 받은 파일의 정보 출력 (실제로는 여기에서 파일을 처리해야 합니다.)
+        System.out.println("Received file: " + file.getOriginalFilename());
+
+        // 클라이언트에 응답
+        return "파일 업로드가 성공했습니다.";
     }
 	
 }
