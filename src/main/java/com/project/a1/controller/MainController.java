b93908600dd54c1f2b8460e5cccedafef3d2ebf1
김범수 <qjatn4793@naver.com>
2023-12-20 14:27:27 +0900

    [2023-12-20] 김범수 불필요 import 제거

--- src/main/java/com/project/a1/controller/MainController.java
+++ src/main/java/com/project/a1/controller/MainController.java
@@ -1,16 +1,13 @@
 package com.project.a1.controller;
 
 import java.io.IOException;
-import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.google.gson.Gson;
@@ -21,13 +18,7 @@ import com.project.a1.vo.SearchResultVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-import org.springframework.http.HttpEntity;
-
 // api 통신
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 
 @Slf4j
